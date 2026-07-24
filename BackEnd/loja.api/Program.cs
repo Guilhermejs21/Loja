34b43012463ca7f384c8fb3ce77880f709f3e10a
Guilhermejs21 <guilermejs77@gmail.com>
2026-07-24 13:41:13 -0300

    add

--- BackEnd/loja.api/Program.cs
+++ BackEnd/loja.api/Program.cs
@@ -4,11 +4,15 @@ using loja.api.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 builder.Services.AddCors(options =>
 {
@@ -30,8 +34,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-
 
 app.UseHttpsRedirection();
 
--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://localhost:7171";
+const API_URL = "http://localhost:5000";
 
 let produtos = [];
 
